37f3e64f6aad80eafb8f3b770e09188029b3bc91
juancarlos <juancarlosenrique103@gmail.com>
2021-05-21 11:41:16 -0500

    Added validations :zap:

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,17 @@
+# frozen_string_literal: true
+
 class Product < ApplicationRecord
+  validates :title, :description, :image_url, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+  validates :title, uniqueness: true
+  validates :title, length: { minimum: 1,
+                              maximum: 250,
+                              too_short: 'debería tener al menos %<count>s caracter',
+                              too_long: 'debería tener menos de %<count>s caracteres' }
+
+  # %r{\.(gif|jpg|png)\z}i
+  validates :image_url, allow_blank: true, format: {
+    with: /\.(gif|jpg|png)/i,
+    message: 'must be a URL for GIF, JPG or PNG image.'
+  }
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,74 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def new_product(image_url)
+    Product.new(title: 'Product 1',
+                description: 'Product description',
+                price: 1,
+                image_url: image_url)
+  end
+
+  test 'product attribute must not be a empty' do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:price].any?
+    assert product.errors[:image_url].any?
+  end
+
+  test 'product price must be positive' do
+    product = Product.new(title: 'New product', description: 'Product on sale', image_url: 'image.png')
+    product.price = -1
+    assert product.invalid?
+    assert ['must be greater than or equal to 0.01'], product.errors[:price]
+    product.price = 0
+    assert product.invalid?
+    assert ['must be greater than or equal to 0.01'], product.errors[:price]
+    product.price = 10
+    assert product.valid?, "#{product.price} no es válido"
+  end
+
+  test 'product title length must be greater than 1' do
+    product = Product.new(description: 'Product on sale', image_url: 'image.png', price: 10)
+    product.title = ''
+    assert product.invalid?
+    product.title = 'New product'
+    assert product.valid?, "#{product.title} no es válido"
+  end
+
+  test 'product title length must be less than 1' do
+    product = Product.new(description: 'Product on sale', image_url: 'image.png', price: 10)
+    product.title = 'a' * 251
+    assert product.invalid?
+    product.title = 'New product'
+    assert product.valid?, "#{product.title} no es válido"
+  end
+
+  test 'image url must be have a format permited' do
+    ok = %w[fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg http://a.b.c/x/y/z/fred.gif]
+    bad = %w[fred.doc fred.more fred.xls]
+
+    ok.each  do |image_url|
+      assert new_product(image_url).valid?, "#{image_url} shouldn't be valid"
+    end
+
+    bad.each do |image_url|
+      assert new_product(image_url).invalid?, "#{image_url} shouldn't be valid"
+    end
+  end
+
+  test 'product is not valid without a unique title' do
+    product = Product.new(title: products(:ruby).title, description: 'yyy', price: 1, image_url: 'image.png')
+    assert product.invalid?
+    assert_equal ['has already been taken'], product.errors[:title]
+  end
+
+  test 'product is not a valid without a unique title - i18n' do
+    product = Product.new(title: products(:ruby).title, description: 'yyy', price: 1, image_url: 'image.png')
+    assert product.invalid?
+    assert_equal [I18n.translate('errors.messages.taken')], product.errors[:title]
+  end
 end
